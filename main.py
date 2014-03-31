da9eb0abde86c44a20174f9baa217219e00df024
frenzymadness <luu@test.cz>
2014-03-31 12:19:52 +0200

    Algoritmus komprese prepsan do funkce s ukladanim do souboru.

--- main.py
+++ main.py
@@ -6,38 +6,51 @@ import re
 # Regularni vyraz  pro parsing radku z logu
 parser = re.compile('(\d+\.\d+\.\d+\.\d+) - - (\[.+ .+\]) (".+") (\d+ \d+) (".+") (".+")')
 
-# nazev souboru pro kompresi
-logfile = 'small.log'
+# Pripona pro komprimovane soubory
+cmp_suffix = '.cmp'
+dcmp_suffix = '.dcmp'
 
-# pomocna pole
-original = []
-output = []
-hash_table = {}
 
-# nacteni vstupnich dat do pole
-with open(logfile, 'r') as f:
-    for line in f:
-        original.append(parser.match(line).groups())
+def compress(logfile):
 
-#print original
+    # pomocna pole
+    original = []
+    output = []
+    hash_table = {}
 
-for row_num, row_content in enumerate(original):
-    output.append([])
-    for part_num, part_content in enumerate(row_content):
-        if len(part_content) > 4:
-            if part_content in hash_table.keys():
-                output[-1].append(hash_table[part_content])
+    # nacteni vstupnich dat do pole
+    with open(logfile, 'r') as f:
+        for line in f:
+            original.append(parser.match(line).groups())
+
+    # Samotna komprese
+    # Pro kazdy radek pridame zaznam do vysledku
+    for row_num, row_content in enumerate(original):
+        output.append([])
+        # Pro kazdou cast jednoho zaznamu
+        for part_num, part_content in enumerate(row_content):
+            # Pokud je cast vetsi nez ctyri znaky, provedeme kompresi
+            if len(part_content) > 4:
+                # pokud cast v hash tabulce, pouzijeme hash, jinak ji
+                # do hash tabulky vlozime a vytvorime pro ni hash
+                if part_content in hash_table.keys():
+                    output[-1].append(hash_table[part_content])
+                else:
+                    hash_table[part_content] = "%d#%d" % (row_num, part_num)
+                    output[-1].append(part_content)
+            # casti mensi nez urcity pocet znaku ignorujeme a jen vkladame do vysledku
             else:
-                hash_table[part_content] = "%d#%d" % (row_num, part_num)
                 output[-1].append(part_content)
-        else:
-            output[-1].append(part_content)
 
-#print output
-for record in original:
-    print " ".join(record)
+    #ulozeni vystupu
+    with open(logfile+cmp_suffix, 'w') as f:
+        for record in original:
+            f.write(" ".join(record))
 
-print "#####################################################"
-
-for record in output:
-    print " ".join(record)
+if __name__ == '__main__':
+    compress('10.log')
+    compress('100.log')
+    compress('1000.log')
+    compress('10000.log')
+    compress('100000.log')
+    compress('200000.log')
